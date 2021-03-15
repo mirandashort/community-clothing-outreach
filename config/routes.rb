2f641fade2167711f6e448f17d3a79f046a9f213
Miranda Short <mird2794@gmail.com>
2021-03-15 14:53:40 -0400

    set up models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'appointment/index'
+  devise_for :users
+  devise_scope :user do
+    get '/logout' => 'devise/sessions#destroy'
+  end
+  root to: 'pages#index'
+
+  get '/', to: 'pages#index'
+
+  resources :appointments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
